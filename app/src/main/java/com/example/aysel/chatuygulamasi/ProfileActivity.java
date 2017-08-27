def9c233b2f10241555d6a117c8fda91d452d2e2
AyselAydin <ayselaydin01@gmail.com>
2017-08-27 21:40:28 +0300

    aysel
    send UserId

---
+++ app/src/main/java/com/example/aysel/chatuygulamasi/ProfileActivity.java
@@ -0,0 +1,20 @@
+package com.example.aysel.chatuygulamasi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    TextView mDisplayID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        String user_id = getIntent().getStringExtra("user_id");
+        mDisplayID = findViewById(R.id.profile_displayName);
+        mDisplayID.setText(user_id);
+    }
+}
